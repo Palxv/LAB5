4ca45020fe3441bea3f9811de37d8fee2e9d5a3f
Palxv <20pashapasha18@gmail.com>
2026-01-21 19:32:04 +0200

    Add files via upload

---
+++ One-Tree/One-Tree/One-Tree.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n, i;
+	cout << "n = "; cin >> n;
+	cout << "Натуральні дільники: ";
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			cout << i << " ";
+		}
+	}
+}
---
+++ two_two/two_two/two_two.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    int n, i, sign;
+    double sum, limit;
+    cout << "n = ";
+    cin >> n;
+    sum = 0.0;
+    sign = 1; 
+    limit = 10.0;
+    for (i = 1; i <= n; i++) {
+        if (i == limit) {
+            sign = -sign;
+            limit = limit * 10;
+        }
+        sum = sum + (1.0 * sign) / i;
+    }
+    cout << "Результат: " << sum << endl;
+    return 0;
+}
